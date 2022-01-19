5332aabdcaa9f5e4228e61c7cf1df21c6bd3a474
alex <allexander0509@gmail.com>
2022-01-19 18:02:38 +0300

    preprocessing: added df_generator for stream-like reading csv-info from .zip data; configured flake8 to 120 line length

--- src/processing/pre_process.py
+++ src/processing/pre_process.py
@@ -1,5 +1,28 @@
 # -*- coding: utf-8 -*-
 
+from typing import Iterator
+from zipfile import ZipFile
+
+import pandas as pd
+
+
+def df_generator(path: str) -> Iterator:
+    """
+    Generator of dataframes for next steps of data processing
+
+    :param path: Path to zip file with csv files
+    :return: Generator of pandas dataframes
+    """
+
+    zip_src = ZipFile(path)
+    tables = zip_src.namelist()
+
+    for table in tables:
+        with zip_src.open(table) as file_csv:
+            yield pd.read_csv(file_csv)
+
 
 if __name__ == "__main__":
-    pass
+    new_gen = df_generator(r"C:\Storage\Coding\EPAM_Traininng\weather_analysis\data\hotels.zip")
+    df = next(new_gen)
+    print()
