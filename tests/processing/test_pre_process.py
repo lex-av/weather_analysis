2c7dbc96cadfa761dc41f6af1c4f17bb5865632f
alex <allexander0509@gmail.com>
2022-01-19 18:57:00 +0300

    processing: successfully tested df_generator

--- tests/processing/test_pre_process.py
+++ tests/processing/test_pre_process.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import pytest
 
-if __name__ == "__main__":
-    pass
+from src.processing.pre_process import df_generator
+
+
+def test_df_generator_read_csv():
+    new_gen = df_generator(r"test_data/hotels_test_data.zip")
+    assert list(next(new_gen).columns) == ["Id", "Name", "Country", "City", "Latitude", "Longitude"]
+
+
+def test_df_generator_read_all():
+    new_gen = df_generator(r"test_data/hotels_test_data.zip")
+    next(new_gen)
+    next(new_gen)
+    with pytest.raises(StopIteration):
+        next(new_gen)
