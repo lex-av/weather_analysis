7450053d969cc8de022be1f5f64a7c4cb17d665f
alex <allexander0509@gmail.com>
2022-01-19 20:38:50 +0300

    processing: added df clearing functionality to df_generator and tested it

--- src/processing/pre_process.py
+++ src/processing/pre_process.py
@@ -9,6 +9,7 @@ import pandas as pd
 def df_generator(path: str) -> Iterator:
     """
     Generator of dataframes for next steps of data processing
+    Drops rows with Nan or incorrect values on the fly
 
     :param path: Path to zip file with csv files
     :return: Generator of pandas dataframes
@@ -19,10 +20,17 @@ def df_generator(path: str) -> Iterator:
 
     for table in tables:
         with zip_src.open(table) as file_csv:
-            yield pd.read_csv(file_csv)
+            df = pd.read_csv(file_csv)
+
+            # Using to_numeric to replace to NaN all invalid values
+            df.Latitude = pd.to_numeric(df.Latitude, errors="coerce")
+            df.Longitude = pd.to_numeric(df.Longitude, errors="coerce")
+            df.dropna(inplace=True)
+            yield df
 
 
 if __name__ == "__main__":
     new_gen = df_generator(r"C:\Storage\Coding\EPAM_Traininng\weather_analysis\data\hotels.zip")
+    next(new_gen)
     df = next(new_gen)
     print()
--- tests/processing/test_pre_process.py
+++ tests/processing/test_pre_process.py
@@ -1,18 +1,32 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 import pytest
 
 from src.processing.pre_process import df_generator
 
 
-def test_df_generator_read_csv():
-    new_gen = df_generator(r"test_data/hotels_test_data.zip")
+@pytest.fixture()
+def get_path():
+    return os.getcwd()  # CWD in configuration has to be root of a project
+
+
+def test_df_generator_read_csv(get_path):
+    new_gen = df_generator(get_path + "/tests/test_data/hotels_test_data.zip")
     assert list(next(new_gen).columns) == ["Id", "Name", "Country", "City", "Latitude", "Longitude"]
 
 
-def test_df_generator_read_all():
-    new_gen = df_generator(r"test_data/hotels_test_data.zip")
+def test_df_generator_read_all(get_path):
+    new_gen = df_generator(get_path + "/tests/test_data/hotels_test_data.zip")
     next(new_gen)
     next(new_gen)
     with pytest.raises(StopIteration):
         next(new_gen)
+
+
+def test_df_generator_clear_csv(get_path):
+    new_gen = df_generator(get_path + "/tests/test_data/hotels_test_data.zip")
+    next(new_gen)  # Skip first file in archive
+    df_with_nan = next(new_gen)
+    assert df_with_nan.City.count() == 1
