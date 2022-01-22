6fb66b530b6427176b623fb94254754e4e813fa8
alex <allexander0509@gmail.com>
2022-01-23 00:24:34 +0300

    connection_utils: added geo_data processing tools to geo_data module, deleted caching module

--- src/connection_utils/caching.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from src.service_utils import project_root
-
-if __name__ == "__main__":
-    print(project_root())
--- src/connection_utils/geo_data.py
+++ src/connection_utils/geo_data.py
@@ -1,5 +1,67 @@
 # -*- coding: utf-8 -*-
 
+from random import randint
+from time import sleep
+from typing import List, Union
+
+import pandas as pd
+from geopy.extra.rate_limiter import RateLimiter
+from geopy.geocoders import Nominatim
+from joblib import Memory
+
+from src.processing.pre_process import df_cleaner, df_generator, df_group_and_filter
+from src.service_utils import project_root
+
+memory = Memory(project_root() / "src" / "cache")
+
+
+@memory.cache
+def get_address_worker(coordinate: str) -> Union[str, None]:
+    """
+    Function to get address from external API
+    Caching results is highly recommended due to API limitations
+
+    :param coordinate: Concatenated Latitude and Longitude. example: "45.7865, -56.9483"
+    :return: Geographical adress for given coordinates or None in case of API errors
+    """
+
+    geocoder = Nominatim(user_agent="weather_hotels_1206")
+    reverse = RateLimiter(geocoder.reverse, min_delay_seconds=1.5, return_value_on_exception=None, max_retries=2)
+    # Small random sleep to help avoiding API TimeOuts
+    sleep(randint(3 * 100, 10 * 100) / 1000)
+    location = reverse(coordinate, language="en")
+
+    return location.address
+
+
+def collect_geo_data(coordinates: pd.DataFrame, max_index: int = None) -> List:
+    """
+    Uses get_address_worker to form list of geographical addresses by given
+    DataFrame coordinates
+
+    :param coordinates: pandas DataFrame
+    :param max_index: index to use slice of DataFrame as such as df[0:max_index]
+    :return: List of addresses for given DataFrame of coordinates
+    """
+
+    address_list = []
+    if max_index is None:
+        max_index = len(coordinates)
+
+    for coordinate in coordinates.values[0:max_index]:
+        address_list.append(get_address_worker(coordinate))
+
+    return address_list
+
 
 if __name__ == "__main__":
-    pass
+    gen = df_generator("C:/Storage/Coding/EPAM_Traininng/weather_analysis/data/hotels.zip")
+
+    df_cleared = df_group_and_filter([df_cleaner(df) for df in gen])
+
+    df_lat_lon = df_cleared[["Latitude", "Longitude"]]
+
+    addr_lst = collect_geo_data(df_lat_lon, max_index=15)
+
+    print()
+    print()
