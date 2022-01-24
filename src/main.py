1ab676a068f459433dfc86dda3da0e529e3d4164
alex <allexander0509@gmail.com>
2022-01-24 14:41:29 +0300

    added main for project and updated requirements.txt with requests

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# from src.processing.pre_process import df_cleaner, df_generator, df_group_and_filter
+# from src.service_utils import project_root
+
+
+def main():
+    """"""
+
+    # data_frames_gen = df_generator(str(project_root() / "data/hotels.zip"))
+    # df_full = df_group_and_filter([df_cleaner(df) for df in data_frames_gen])
+
+
+if __name__ == "__main__":
+    main()
