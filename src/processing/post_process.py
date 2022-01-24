1cb10498eb830f95effd530caae2b5545f6ecad3
alex <allexander0509@gmail.com>
2022-01-24 15:19:37 +0300

    postprocessing: added function to generate centres-detaframe and successfully tested it

--- src/processing/post_process.py
+++ src/processing/post_process.py
@@ -1,5 +1,37 @@
 # -*- coding: utf-8 -*-
 
+import pandas as pd
+
+from src.connection_utils.geo_data import calc_centre
+from src.processing.pre_process import df_cleaner, df_generator, df_group_and_filter
+from src.service_utils import project_root
+
+
+def generate_centres_df(cities_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Generates DataFrame of city centres and their coordinates
+
+    :param cities_df: pandas DataFrame of City/Hotels data
+    :return: DataFrame of Cities and their centre coordinates
+    """
+
+    data_dict = {"City": list(cities_df["City"].unique())}
+    centre_list = []
+
+    for city in data_dict["City"]:
+        centre_list.append(calc_centre(cities_df[cities_df["City"] == city]))
+
+    data_dict["Latitude"] = [centre[0] for centre in centre_list]
+    data_dict["Longitude"] = [centre[1] for centre in centre_list]
+    df_centres = pd.DataFrame.from_dict(data_dict)
+
+    return df_centres
+
 
 if __name__ == "__main__":
-    pass
+    data_frames_gen = df_generator(str(project_root() / "data/hotels.zip"))
+    df_full = df_group_and_filter([df_cleaner(df) for df in data_frames_gen])
+
+    generate_centres_df(df_full)
+
+    print()
---
+++ tests/processing/test_post_process.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from src.processing.post_process import generate_centres_df
+from src.processing.pre_process import df_cleaner, df_generator, df_group_and_filter
+
+
+def test_df_generator_read_csv(get_path):
+    data_frames = df_generator(get_path + "/tests/test_data/hotels_test_data.zip")
+    df_full = df_group_and_filter([df_cleaner(df) for df in data_frames])
+    centre_df = generate_centres_df(df_full)
+
+    assert list(centre_df.keys()) == ["City", "Latitude", "Longitude"]
